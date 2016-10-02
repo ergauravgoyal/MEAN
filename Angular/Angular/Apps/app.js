168c5003d5706e6b9d19a559c967ffef7082099d
gaura <gaura@DESKTOP-KHLD3V2>
2016-10-02 21:51:14 +0530

    MEAN Stack Practice

---
+++ Angular/Angular/Apps/app.js
@@ -0,0 +1,31 @@
+﻿var app = angular.module("flapperNews", []);
+app.controller('MainCtrl', ['$scope', function ($scope) {
+    $scope.test = "Hello World !";
+    $scope.posts = [
+        { title: 'post 1', upvotes: 5 },
+        { title: 'post 2', upvotes: 9 },
+        { title: 'post 3', upvotes: 8 },
+        { title: 'post 4', upvotes: 2 }
+    ];
+    $scope.addPost = function () {
+        if (!$scope.title || $scope.title === '') {
+            return;
+        }
+        upvotes = 0;
+        $scope.posts.push({
+            title: $scope.title,
+            link: $scope.link,
+            upvotes: 0
+        });
+        $scope.title = "";
+        $scope.link = "";
+
+    };
+
+    $scope.incrementUpvotes=function(post) {
+        post.upvotes+=1;
+    };
+
+    $scope.source = "This demo is created with the help of ";
+    $scope.sourceLink = "https://thinkster.io/mean-stack-tutorial";
+}]);
