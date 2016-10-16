7dfed6a19386e119ef3a494bd0b24290510d377f
gaura <gaura@DESKTOP-KHLD3V2>
2016-10-16 18:38:50 +0530

    Solution Added

---
+++ MAD STreetDan/MAD STreetDan/Scripts/angularappscripts/app.js
@@ -0,0 +1,8 @@
+﻿var app = angular.module("myApp", []);
+
+app.controller('MainCtrl', ['$scope', '$http', function ($scope,$http) {
+    $http.get("https://hackerearth.0x10.info/api/fashion?type=json&query=list_products").then(function (response) {
+        $scope.myData = response.data.products;
+    });
+    $scope.Name="gaurav"
+}]);
