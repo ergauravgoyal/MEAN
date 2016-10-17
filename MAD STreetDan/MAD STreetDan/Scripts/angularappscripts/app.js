2176d43c05cefd2aae69c7abe1a9bc398273f338
gaura <gaura@DESKTOP-KHLD3V2>
2016-10-18 00:01:10 +0530

    Commit

--- MAD STreetDan/MAD STreetDan/Scripts/angularappscripts/app.js
+++ MAD STreetDan/MAD STreetDan/Scripts/angularappscripts/app.js
@@ -3,6 +3,7 @@
 app.controller('MainCtrl', ['$scope', '$http', function ($scope,$http) {
     $http.get("https://hackerearth.0x10.info/api/fashion?type=json&query=list_products").then(function (response) {
         $scope.myData = response.data.products;
+        alert($scope.myData[1].color);
     });
     $scope.Name="gaurav"
 }]);
